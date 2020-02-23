e74c21cf380ff4f12eb4dd3898c23988e3f05058
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-02-23 15:35:26 -0800

    add the displayList() method to display the contact names

--- src/adt/MyProgramADT.java
+++ src/adt/MyProgramADT.java
@@ -16,4 +16,6 @@ public interface MyProgramADT {
     //delete() method to delete a contact from the contact list
     void delete(int item);
 
+    //displayList() method to display the contact names
+    void displayList();
 }
