c72983f4cb0141465208675c5bf5398cf51cc649
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-02-23 15:29:31 -0800

    add fields FName for first name, LName for last name, contact list for contacts, email for email addresses in the person class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,4 +1,13 @@
 package definition;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class Person {
+
+    // these are the fields for the person class
+    private String FName;
+    private String LName;
+    private ArrayList<BigInteger> contactList = new ArrayList<>();
+    private String email;
 }
