d9819ffbc2f69539af5d9c5afa58102598eeac5f
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-02-23 15:40:07 -0800

    add the head reference variable will store the reference to the first contact of the contact list.

--- src/definition/MyProgram.java
+++ src/definition/MyProgram.java
@@ -3,4 +3,32 @@ package definition;
 import adt.MyProgramADT;
 
 public class MyProgram implements MyProgramADT {
+
+    // This reference variable will store the reference to the first contact of the contact list.
+    private Node head = null;
+
+    @Override
+    public void add(Person item) {
+
+    }
+
+    @Override
+    public void view() {
+
+    }
+
+    @Override
+    public void search(String item) {
+
+    }
+
+    @Override
+    public void delete(int item) {
+
+    }
+
+    @Override
+    public void displayList() {
+
+    }
 }
